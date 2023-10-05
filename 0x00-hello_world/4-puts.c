1d00c20aa3aa6b8dca54e8cae9c94f9c9c313a22
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-10-05 15:45:17 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function where code begins
+ *
+ * Return: 0 when succeesful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
