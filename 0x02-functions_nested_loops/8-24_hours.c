4a86bb2a78d32a76f3cfbdc352cfec2a68870d79
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-10-11 12:21:03 -0700

    every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	{
 		for (minutes = 0 ; minutes < 60; minutes++)
 		{
-			_putchar(hour % 10 + '0');
 			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
 			_putchar(':');
-			_putchar(minutes % 10 + '0');
 			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
 			_putchar('\n');
 		}
 	}
