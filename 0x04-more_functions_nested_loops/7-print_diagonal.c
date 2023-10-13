505445b4c9f74d56c7a2c81eca5f67bcc83a7155
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-10-13 13:17:57 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,9 @@ void print_diagonal(int n)
 	int a, b;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (a = 1 ; a <= n ; a++)
@@ -18,7 +20,9 @@ void print_diagonal(int n)
 			if (a > 1)
 			{
 				for (b = 1 ; b <= a -1 ; b++)
+				{
 					_putchar(' ');
+				}
 			}
 			_putchar('\\');
 			_putchar('\n');
