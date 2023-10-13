f8341c9b2d37d7d59c199e2a32ed89943cec7332
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-10-13 05:33:42 -0700

    0 to 14 x10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 
 	for (a = 0 ; a < 10 ; a++)
 	{
-		for (b = 0 ; b < 15 ; b++);
+		for (b = 0 ; b < 15 ; b++)
 			_putchar(b / 10 + '0');
-			_putchar(b % 10 +'0');
+			_putchar(b % 10 + '0');
 	}
 	_putchar('\n');
 }
