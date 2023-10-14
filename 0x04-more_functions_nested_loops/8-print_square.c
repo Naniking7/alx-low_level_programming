3ed8a72db183bca0d185722997251d8054b13fda
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-10-13 22:44:58 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,20 +8,16 @@
 
 int print_square(int size)
 {
-	int a, b;
-
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
+		int a, b;
+
 		for (a = 1 ; a <= size ; a++)
 		{
 			for (b = 1 ; b <= size ; b++)
-			{
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
