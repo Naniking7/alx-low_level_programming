19eda874155e2f160d205cbc5887ce8f3939865f
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-10-13 23:51:59 -0700

    trinagles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - prints diagonals in the '#' character
+ * @size: parameter to represent size
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 1 ; a<= size ; a++)
+		{
+			for (c = size - a ; c >= 1 ; c--)
+				_putchar(' ');
+			for (b = 1 ; b <= a ; b++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
