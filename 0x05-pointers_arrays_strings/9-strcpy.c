e002317496200e48f8a96857fd4409c18282e5bc
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-10-16 21:23:37 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_strcpy - copies the strings pointed by 'src' including end nullbyte to the buffer pointed to by 'dest'
+ *_strcpy - copies the string pointed by 'src' including the end nullbyte
  *@dest: pointer(parameter) to string to be copied to
  *@src: pointer(parameter) to string to be copied.
  *
