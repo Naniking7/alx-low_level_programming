decca07a804da261d90cb31a114e3bf26c276589
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-11-10 13:05:56 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments passed to this program
+ * @argc: number of arguments
+ * @argv: actual arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
