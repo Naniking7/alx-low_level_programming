8c43a8b4c5285f77106bb2f34e8b10b665752fa2
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-11-10 12:36:45 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main -  prints this program's name
+ *@argc: number of arguments
+ *@argc: array that stores the actual arguments
+ *Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
