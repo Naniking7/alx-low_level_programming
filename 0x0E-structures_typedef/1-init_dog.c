2bf996052f63cd2fe3f39234546edab3e4e91b56
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-11-10 16:54:19 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initailizes a dog
+ * @d: pointer to dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
