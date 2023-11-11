0f47c6b9967e73c7d71379c529a9becdb64a112e
Philemon S.P <oseisarpongphilemon@gmail.com>
2023-11-10 18:02:37 -0800

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,22 +6,14 @@
  * @name: name of dog
  * @age: dog's age
  * @owner: dog's owner
- *
- * Description: Longer description
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- *  * dog_t - typedef for struct dog
- *   */
-typedef struct dog dog_t;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
